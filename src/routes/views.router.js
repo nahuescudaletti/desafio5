2823f9499e2e7ad17304274806bf7ff661e58f0a
nahuescudaletti <nahueby17@gmail.com>
2023-08-07 14:15:39 -0300

    products with mongoDB

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,13 +1,30 @@
 import { Router } from 'express';
+import Product from '../models/product.js';
 
-const router = Router ();
+const router = Router();
 
 router.get('/realTimeProducts', (req, res) => {
-    res.render('realTimeProducts');
-})
+  res.render('realTimeProducts');
+});
 
 router.get('/home', (req, res) => {
-    res.render('home');
-})
+  res.render('home');
+});
+
+router.get('/products', async (req, res, next) => {
+  try {
+    const allProducts = await Product.find().lean().exec();
+    res.render('productsMongoDB', { all: allProducts });
+  } catch (error) {
+    next(error);
+  }
+
+//    try {
+//         let allProducts = await Product.find().toArray();
+//         res.render('productsMongoDB', { all: allProducts });
+//     } catch (error) {
+//         res.status(500).send('Error al obtener los productos.');
+//     }
+});
 
 export default router;
